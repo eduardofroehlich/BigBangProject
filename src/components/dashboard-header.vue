c8e99837fbd4e8b2751773ea08f9c20cd034f8f0
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 11:49:38 -0300

    Importar DashboardNav para o header
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/components/dashboard-header.vue
+++ src/components/dashboard-header.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import BigBangLogo from './big-bang-logo.vue'
+import DashboardNav from './dashboard-nav.vue'
+import { faBell } from '@fortawesome/free-regular-svg-icons';
 </script>
 
 <template>
-  <div class="bg-primary w-full h-2/5">
-    <BigBangLogo />
+  <div class="bg-primary w-full h-2/5 flex justify-between p-5">
+    <div class="">
+      <BigBangLogo />
+    </div>
+    <div class="">
+      <DashboardNav />
+    </div>
+    <div class="text-end">
+      <FontAwesomeIcon :icon=faBell class="text-white text-2xl" />
+    </div>
   </div>
 </template>
 
