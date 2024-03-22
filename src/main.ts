8a37e6727befb1eba787895a51fa826802df2ac0
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 17:48:57 -0300

    Adicionar iconsax no projeto
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createI18n } from 'vue-i18n'
 import { pt_BR } from './translations/pt_BR';
+import { VsxIcon } from "vue-iconsax";
 
 const i18n = createI18n({
     legacy: false,
@@ -15,5 +16,6 @@ const i18n = createI18n({
 
 const app = createApp(App)
 app.use(i18n)
+app.component("VsxIcon", VsxIcon);
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
