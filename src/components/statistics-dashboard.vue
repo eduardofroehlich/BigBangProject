64684e0327f0e8c21c4111adc640699e87866beb
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 18:02:10 -0300

    Adicionar os cards ao Painel de estatísticas
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/components/statistics-dashboard.vue
+++ src/components/statistics-dashboard.vue
@@ -6,10 +6,24 @@ const { t } = useI18n();
 </script>
 
 <template>
-    <div class="flex gap-10">
-        <Card :title="t('account_pj')" :value="'39,882'" />
-        <Card :title="t('receipts')" :value="'765,88'" />
-        <Card :title="t('profit')" :value="'15,887'" />
+    <div class="flex flex-col">
+        <div class="flex items-center">
+            <div class="pb-7 text-white text-xl flex">
+                <div class="pr-6">
+                    <VsxIcon iconName="Bubble" color="#FF4F4F" size="24" />
+                </div>
+                {{  t('statistics_dashboard') }}
+            </div>
+            <div class="pb-7 text-[#A3A3A3] text-sm pl-4">
+                {{  t('last_days') }}
+            </div>
+        </div>
+        
+        <div class="flex gap-10">
+            <Card :title="t('account_pj')" :value="'39,882'" />
+            <Card :title="t('receipts')" :value="'765,88'" />
+            <Card :title="t('profit')" :value="'15,887'" />
+        </div>
     </div>
 </template>
 
@@ -17,4 +31,6 @@ const { t } = useI18n();
     account_pj: Conta PJ
     receipts: Recebimentos
     profit: Lucro
+    statistics_dashboard: Painel de estatísticas
+    last_days: últimos 7 dias
 </i18n>
