8cced5d8abafd5e01422c8a00774e39bcee9760e
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 18:05:04 -0300

    Adicionar statisticsDashboardd ao index
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/views/dashboard-index.vue
+++ src/views/dashboard-index.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import DashboardHeader from '../components/dashboard-header.vue'
 import BalanceResume from '../components/balance-resume.vue'
+import StatisticsDashboard from '../components/statistics-dashboard.vue'
 </script>
 
 <template>
   <div class="h-screen w-screen">
-    <div class="bg-primary h-2/5">
+    <div class="bg-primary h-4/6">
       <DashboardHeader />
 
       <div>
         <BalanceResume />
+      </div>
+
+      <div class="flex justify-center pt-10">
+        <StatisticsDashboard />
       </div>
     </div>
   </div>
