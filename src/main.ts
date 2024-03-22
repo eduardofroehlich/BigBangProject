9e9fd248cc3740d8ba8a686aa434d8f7118540b8
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 00:23:00 -0300

    Adicionar fontAwesome no projeto
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
