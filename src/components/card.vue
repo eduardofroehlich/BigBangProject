f957485b479ac3d4dd28745608b08d3f7d515ea9
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 17:29:17 -0300

    Finalizar componente de card
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
-const { t } = useI18n();
+import Button from './button.vue';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+const { t } = useI18n();
 </script>
 
 <template>
@@ -12,13 +15,25 @@ const { t } = useI18n();
         <div class="text-2xl pt-6">
             {{ t('currency', {value: '39,882'}) }}
         </div>
+        <div class="flex pt-11">
+            <Button class="w-64" variant="secondary-transparent">
+                <div class="text-sm">
+                    {{ t('more_details') }}
+                </div>
+            </Button>
+            <Button class="w-20 ml-4" variant="secondary-transparent">
+                <div class="text-[#292D32]">
+                    <FontAwesomeIcon :icon=faBars class="" />
+                </div>
+            </Button>
+        </div>
     </div>
 </template>
 
 <style>
 .card-container {
-    @apply flex flex-col items-stretch p-5;
-    @apply w-80 rounded-lg;
+    @apply flex flex-col shadow-md p-5;
+    @apply w-80;
     @apply bg-white border border-solid border-gray-50;
     @apply border border-solid border-gray-100;
 }
@@ -26,4 +41,5 @@ const { t } = useI18n();
 
 <i18n lang="yaml" locale="pt_BR">
     account_pj: Conta PJ
+    more_details: Ver mais detalhes
 </i18n>
