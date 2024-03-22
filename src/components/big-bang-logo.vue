b053e3fb69088799a97541f9d537b457476a350a
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 01:41:59 -0300

    Criar logo da bigBang
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

---
+++ src/components/big-bang-logo.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="font-bold italic text-xl logo-big-bang" >
+        BigBang
+    </div>
+</template>
+
+<style scoped>
+.logo-big-bang {
+    position: absolute;
+    width: 87px;
+    height: 30px;
+    left: 27px;
+    top: 21px;
+
+    background: linear-gradient(270deg, #FF9999 0%, #FF3030 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+}   
+</style>
+
--- src/components/dashboard-header.vue
+++ src/components/dashboard-header.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import BigBangLogo from './big-bang-logo.vue'
 </script>
 
 <template>
   <div class="bg-primary w-full h-2/5">
+    <BigBangLogo />
   </div>
 </template>
 
