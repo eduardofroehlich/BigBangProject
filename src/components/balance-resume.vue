a5a46ce2b1a218c8e9f4377e4297c8c7714ce8ea
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 16:51:02 -0300

    Adicionar os botões de enviar dinheiro e plus no balanceResume
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/components/balance-resume.vue
+++ src/components/balance-resume.vue
@@ -1,24 +1,46 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { useI18n } from 'vue-i18n';
-import { faBolt } from '@fortawesome/free-solid-svg-icons';
-
+import { faBolt, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import Button from './button.vue';
 const { t } = useI18n();
 </script>
 
 <template>
-    <div class="pl-36 pt-16">
+    <div class="px-36 pt-16">
         <div class="text-white text-xl">
             {{ t('total_balance') }}
         </div>
-        <div class="flex items-center">
-            <span class="text-white text-4xl" >
-                {{ t('currency', { value:'304,78' }) }}
-            </span>
-            <FontAwesomeIcon :icon=faBolt class="text-yellow-300 text-2xl px-4" />
-            <span class="text-white text-sm font-normal" >
-                {{ t('yielding', { value: 124 }) }}
-            </span>
+        <div class="flex items-center justify-between">
+            <div>
+                <span class="text-white text-4xl" >
+                    {{ t('currency', { value:'304,78' }) }}
+                </span>
+                <FontAwesomeIcon :icon=faBolt class="text-yellow-300 text-2xl px-4" />
+                <span class="text-white text-sm font-normal" >
+                    {{ t('yielding', { value: 124 }) }}
+                </span>
+            </div>
+            <div class="flex">
+                <Button class="mr-5">
+                    <div class="flex items-center w-52 mx-6">
+                        <div class="text-white pr-10">
+                            <FontAwesomeIcon :icon=faPaperPlane class="text-xl"/>
+                        </div>
+                        <div class="text-sm">
+                            {{ t('send_money') }}
+                        </div>
+                    </div>
+                </Button>
+                <Button>
+                    <div class="flex items-center mx-6">
+                        <div class="text-white">
+                            <FontAwesomeIcon :icon=faPlus class="text-xl"/>
+                        </div>
+                    </div>
+                </Button>
+            </div>
         </div>
     </div>
 </template>
@@ -27,4 +49,5 @@ const { t } = useI18n();
     total_balance: Saldo total
     balance_value: "304,78"
     yielding: "rendendo {value}%"
+    send_money: Enviar dinheiro
 </i18n>
