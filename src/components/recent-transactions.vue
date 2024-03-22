ec467d79cf3e7d4f04d41493bd8ab7ebb0727374
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 18:39:54 -0300

    Adicionar os TransactionItem ao componente de transações recentes
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/components/recent-transactions.vue
+++ src/components/recent-transactions.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
-
+import TransactionItem from './transaction-item.vue';
 const { t } = useI18n();
 </script>
 
@@ -14,14 +14,25 @@ const { t } = useI18n();
             <div class="text-[#A3A3A3] text-sm pl-3.5">
                 {{  t('last_days') }}
             </div>
-            <div class="text-[#7986FE] text-sm pl-14">
+            <div class="text-[#7986FE] text-sm pl-14 cursor-pointer">
                 {{  t('see_all') }}
             </div>
         </div>
-
+        <div class="pt-7 pb-60">
+            <TransactionItem :title="t('mercadinho')" :value="'10,98'" variant="yellow" />
+            <TransactionItem :title="t('tenda')" :value="'26,38'"  variant="red"/>
+            <TransactionItem :title="t('farmacia')" :value="'43,17'"  variant="green" />
+            <TransactionItem :title="t('academia')" :value="'62,12'"  variant="purple" />
+            <TransactionItem :title="t('shop_online')" :value="'135,24'" :borderBottom="true" variant="pink" />
+        </div>
     </div>
 </template>
 
 <i18n lang="yaml" locale="pt_BR">
     recent_transactions: Transações recentes
+    mercadinho: Mercadinho da Cidade
+    tenda: Tenda Nostra
+    farmacia: Fármacia Um
+    academia: Academia Body
+    shop_online: ShopOnline
 </i18n>
