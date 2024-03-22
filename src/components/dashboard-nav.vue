65a6400ec56ba3726b4993cb20b1f623a6703225
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 12:21:59 -0300

    Ajustar para DashboardNav usar nesting no style
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

--- src/components/dashboard-nav.vue
+++ src/components/dashboard-nav.vue
@@ -5,21 +5,32 @@ const { t } = useI18n();
 </script>
 
 <template>
-    <div class="grid grid-cols-4 text-white text-center h-10 items-center">
-        <div class="bg-secondary w-32">
+    <div class="grid grid-cols-4 text-white text-center items-center">
+        <div class="bg-secondary nav-item">
             {{ t('dashboard')}}
         </div>
-        <div class="w-32">
+        <div class="nav-item">
             {{ t('payments')}}
         </div>
-        <div class="w-32">
+        <div class="nav-item">
             {{ t('invoices')}}
         </div>
-        <div class="w-32"> 
+        <div class="nav-item"> 
             {{ t('insights')}}
         </div>
     </div>
 </template>
+
+<style scoped>
+.nav-item {
+    @apply w-32 h-10 flex justify-center items-center;
+
+    &:hover {
+        @apply cursor-pointer border-secondary border-solid border-2;
+    }
+}
+</style>
+
 
 <i18n lang="yaml" locale="pt_BR">
     dashboard: Dashboard
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,22 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      mobile: '360px',
+      tablet: '740px',
+      laptop: '1280px',
+      desktop: '1440px',
+    },
     extend: {
       backgroundColor: {
         'primary': '#202027',
         'secondary': '#FF6969',
+      },
+      borderColor:{
+        'secondary': '#FF6969',
+      },
+      height: {
+        screen: '100dvh',
       },
     },
   },
