351cd22c13030d18e03ad5f04add3bc79002b0bf
Eduardo Froehlich <eduardosilvafroehlich@gmail.com>
2024-03-22 17:37:03 -0300

    Criar componente para agrupar os cards com as estatisticas
    Signed-off-by: Eduardo Froehlich <eduardosilvafroehlich@gmail.com>

---
+++ src/components/statistics-dashboard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import Card from './card.vue';
+
+const { t } = useI18n();
+</script>
+
+<template>
+    <div class="flex gap-10">
+        <Card :title="t('account_pj')" :value="'39,882'" />
+        <Card :title="t('receipts')" :value="'765,88'" />
+        <Card :title="t('profit')" :value="'15,887'" />
+    </div>
+</template>
+
+<i18n lang="yaml" locale="pt_BR">
+    account_pj: Conta PJ
+    receipts: Recebimentos
+    profit: Lucro
+</i18n>
